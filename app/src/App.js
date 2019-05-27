ce302ebc3685a97cd85147b268ec8606972d82a9
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-05-27 09:46:18 -0300

    adicionando reset.css

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+
+import LogoBotcamp from "./components/LogoBotcamp";
 import BtnBotCamp from "./components/BtnBotCamp";
+
+import "./styles/reset.css";
 
 class App extends Component {
     render() {
-        return <BtnBotCamp />
+        return (
+            <>
+                <LogoBotcamp />
+                <BtnBotCamp />
+            </>
+        )
     }
 }
 
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import logoBotCamp from "../../img/botcamp.svg";
+import "./style.css";
+
+const LogoBotcamp = () => {
+    return <img className="logo-botcamp" src={logoBotCamp} alt="Logo do Botcamp" />;
+}
+
+export default LogoBotcamp;
