12e5979b7cbb8ef848d960f6b5d41e3c961af2ab
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-05-28 10:18:36 -0300

    BtnBotcamp usando syled-components

--- app/src/components/BtnBotCamp/index.js
+++ app/src/components/BtnBotCamp/index.js
@@ -1,6 +1,14 @@
-import React from "react";
-import "./style.css";
+import styled from "styled-components";
 
-const BtnBotCamp = ({content}) => <button className="btn-botcamp">{content}</button>
-
+const BtnBotCamp = styled.button.attrs({
+    children: props => props.content
+})`
+    width: 280px;
+    height: 50px;
+    border-radius: 25px;
+    font-size: 20px;
+    color: #ffffff;
+    background-color: #ee3e25;
+    text-transform: uppercase;
+`;
 export default BtnBotCamp;
