05ebc8a970af79212d93e0d5999c234028141581
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-05-29 15:45:33 -0300

    Criando o component HeaderWrapper

--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import LogoBotcamp from "../../components/LogoBotcamp";
 import LogoutBotcamp from "../../components/LogoutBotcamp";
+import HeaderWrapper from "../../components/HeaderWrapper";
 
 const Chat = () =>
-    <>
+    <HeaderWrapper>
         <LogoBotcamp />
         <LogoutBotcamp />
-    </>
+    </HeaderWrapper>
 
 export default Chat;
