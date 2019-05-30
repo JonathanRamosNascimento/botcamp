2b24b7907722061c3a6873343b2fe61942932c38
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-05-30 09:15:51 -0300

    controle de rotas

--- app/src/components/HeaderWrapper/index.js
+++ app/src/components/HeaderWrapper/index.js
@@ -3,6 +3,20 @@ import styled from "styled-components";
 const HeaderWrapper = styled.header`
     background-color: #26265e;
     padding: 10px 16px;
+    display: flex;
+    justify-content: space-between;
+    position: relative;
+
+    ::after{
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        transform: translateY(100%);
+        border: 0px solid transparent;
+        border-top: 14px solid #26265e;
+        border-left-width: 100vw;
+    }
 `;
 
 export default HeaderWrapper;
--- app/src/pages/Chat/index.js
+++ app/src/pages/Chat/index.js
@@ -3,11 +3,15 @@ import React from "react";
 import LogoBotcamp from "../../components/LogoBotcamp";
 import LogoutBotcamp from "../../components/LogoutBotcamp";
 import HeaderWrapper from "../../components/HeaderWrapper";
+import InputMessage from "../../components/InputMessage";
 
 const Chat = () =>
-    <HeaderWrapper>
-        <LogoBotcamp small />
-        <LogoutBotcamp />
-    </HeaderWrapper>
+    <>
+        <HeaderWrapper>
+            <LogoBotcamp small />
+            <LogoutBotcamp />
+        </HeaderWrapper>
+        <InputMessage />
+    </>
 
 export default Chat;
