2506f834c8b03cb646258eb2071035a74abac4ec
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-05-30 06:16:26 -0300

    Diminuir o HeaderWrapper

--- app/src/components/LogoutBotcamp/index.js
+++ app/src/components/LogoutBotcamp/index.js
@@ -5,6 +5,8 @@ import logout from "../../img/logout.png";
 const LogoutBotcamp = styled.img.attrs({
     src: logout,
     alt: "Icone para sair"
-})``
+})`
+    float: right;
+`
 
 export default LogoutBotcamp;
